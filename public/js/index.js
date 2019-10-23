1afe298d5c184675fe459b9194cfd2566e23a3f0
Ryan Holt <ryanholt22@yahoo.com>
2019-10-22 21:38:39 -0500

    rebuilding frontend

--- public/js/index.js
+++ public/js/index.js
@@ -91,3 +91,20 @@ submitBtnEl.addEventListener("click", handleFormSubmit);
 document.querySelectorAll(".delete").forEach(btn => {
   btn.addEventListener("click", handleDeleteBtnClick);
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 1500); // Change image every 2 seconds
+}
