2b5e15e9080e55b253bf26025fc67b8a65615861
cmitchell <clmitchell630@gmail.com>
2019-10-26 13:43:17 -0500

    able to console log array, now to get it to display through handlebars

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var axios = require("axios");
 
 module.exports = function(app) {
   // Get all examples
@@ -15,6 +16,34 @@ module.exports = function(app) {
     });
   });
 
+  app.post("/api/gallery", function(req, res) {
+    var search = res.body;
+    let query =
+      "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=" +
+      search;
+    axios.get(query).then(function(response) {
+      // console.log(response.data.objectIDs);
+      let artArr = response.data.objectIDs;
+      let imgArr = [];
+      let callCount = 0;
+      //console.log(artArr);
+      artArr.forEach(function(item, index) {
+        //console.log(index + " " + item);
+        let imgQuery =
+          "https://collectionapi.metmuseum.org/public/collection/v1/objects/" +
+          item;
+        //console.log(imgQuery);
+        axios.get(imgQuery).then(function(response) {
+          imgArr.push(response.data.primaryImage);
+          callCount++;
+          if (callCount === artArr.length) {
+            console.log(imgArr);
+            res.send(imgArr);
+          }
+        });
+      });
+    });
+  });
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(
